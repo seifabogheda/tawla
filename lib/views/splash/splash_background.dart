f7dd3b9f80e6f8be35a9e802cc80f82964aee268
Seif Abogheda <47884997+seifabogheda@users.noreply.github.com>
2020-08-25 03:34:22 +0200

    Add files via upload

---
+++ lib/views/splash/splash_background.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SplashBackground extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      child: Image(
+        image: AssetImage('assets/images/background.png'),
+        fit: BoxFit.fill,
+      ),
+    );
+  }
+}
